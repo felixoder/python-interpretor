6067969ce614fc2367876fbd4f4b53cbb4783ddb
Debayan <debayanghosh408@gmail.com>
2024-03-04 02:58:31 +0530

    handled the static files

--- api/index.js
+++ api/index.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const { spawn } = require('child_process');
 const bodyParser = require('body-parser');
-
+const path = require('path')
 const app = express();
 const port = 3000;
 
 // Middleware to parse JSON bodies
 app.use(express.json());
 app.use(bodyParser.json());
+
+// Serve static files from the client/dist directory
+app.use(express.static(path.join(__dirname, '/client/dist')));
 
 // Route to handle compilation and execution of Python code
 app.post('/api/run-python', (req, res) => {
@@ -36,6 +39,11 @@ app.post('/api/run-python', (req, res) => {
     // Send the output data to the client
     res.json({ output: dataToSend });
   });
+});
+
+// Route to serve index.html for any other requests
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
 });
 
 // Start the server
